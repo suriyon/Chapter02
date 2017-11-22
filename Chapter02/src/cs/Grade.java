3ba677a9adf6cc193d5155ce354d0791e5e2ac8a
suriyon <suriyon.s@ubru.ac.th>
2017-11-22 14:13:22 +0700

    แก้ไขไฟล์ Grade.java ให้แสดงชื่อนักเรียนว่าได้เกรดอะไร

--- Chapter02/src/cs/Grade.java
+++ Chapter02/src/cs/Grade.java
@@ -22,15 +22,15 @@ public class Grade {
 			int Score = Integer.parseInt(tmp);
 			if(Score >= 0 && Score <= 100) {
 				if(Score >= 80)
-					System.out.println("เกรดที่ได้คือ A");
+					System.out.println(StdName + " : เกรดที่ได้คือ A");
 				else if(Score >= 70)
-					System.out.println("เกรดที่ได้คือ B");
+					System.out.println(StdName + " : เกรดที่ได้คือ B");
 				else if(Score >= 60)
-					System.out.println("เกรดที่ได้คือ C");
+					System.out.println(StdName + " : เกรดที่ได้คือ C");
 				else if(Score >= 50)
-					System.out.println("เกรดที่ได้คือ D");
+					System.out.println(StdName + " : เกรดที่ได้คือ D");
 				else
-					System.out.println("เกรดที่ได้คือ F");
+					System.out.println(StdName + " : เกรดที่ได้คือ F");
 			}
 			System.out.println();
 			System.out.print("ต้องการทำงานต่อหรือไม่? [Y/N] : ");
